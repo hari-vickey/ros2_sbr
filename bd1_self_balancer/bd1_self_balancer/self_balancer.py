05fb6032b35c8b3b0895c7ef1dae7281d716fc2f
hari-vickey <harivikinesh@gmail.com>
2022-08-15 16:53:44 +0530

    Updated Code

--- bd1_self_balancer/bd1_self_balancer/self_balancer.py
+++ bd1_self_balancer/bd1_self_balancer/self_balancer.py
@@ -25,13 +25,17 @@ class SelfBalancingBot(Node):
         # Initializing Node
         super().__init__('self_balancer')
         # PID Variables
-        self.set_pos = 0.0
-        self.Kp, self.Ki, self.Kd = 16.5, 1e-5, 350
+        self.set_pos, self.limit = 0.0, 100
+        self.Kp, self.Ki, self.Kd = 33, 1e-5, 50
         self.curr_pos, self.prev_error, self.error_sum = 1e-4, 0, 0
 
         self.publisher = self.create_publisher(Twist, '/cmd_vel', 1)
         self.create_subscription(Odometry, '/odom', self.pos_callback, 1)
         self.velocity = Twist()
+        # Initializing Velocity to zero
+        self.velocity.linear.x, self.velocity.linear.y = 0.0, 0.0
+        self.velocity.linear.z, self.velocity.angular.x = 0.0, 0.0
+        self.velocity.angular.y, self.velocity.angular.z = 0.0, 0.0
 
     # Position Callback
     def pos_callback(self, msg):
@@ -39,59 +43,70 @@ class SelfBalancingBot(Node):
         callback function for ROS2 Subscriber to the topic /odom
         """
         orientation_list = [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
-             msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
+                            msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
 
         # Converting Quaternion to euler angles
         self.curr_pos, p, y = euler_from_quaternion(orientation_list)
 
         # Computing Error (for Proportional Term)
-        self.pitch_error = self.curr_pos - self.set_pos
+        self.error = self.curr_pos - self.set_pos
 
         # Computing Change in Error (For Derivative Term)
-        self.change_in_error = self.pitch_error - self.prev_error
-
-        # Computing the output of controller
-        self.velocity.linear.x = ((self.Kp * self.pitch_error) + \
-                                 (self.Ki * self.error_sum) + \
-                                 (self.Kd * self.change_in_error))
+        self.change_in_error = self.error - self.prev_error
 
         # Computing Sum of errors (For Integral Term)
-        self.error_sum += self.pitch_error
+        self.error_sum += self.error
 
-        # Constraining Error Sum in the range -300 to 300
-        if int(self.error_sum) not in range(-300, 300):
+        # Limiting Error Sum
+        if int(self.error_sum) not in range(-self.limit, self.limit):
             if self.error_sum <= 0:
-                self.error_sum = self.error_sum + 300
+                self.error_sum = self.error_sum + self.limit
             else:
-                self.error_sum = self.error_sum - 300
+                self.error_sum = self.error_sum - self.limit
 
-        # Updating pervious errors
-        self.prev_error = self.pitch_error
+        # Computing the output of controller
+        if abs(self.error) > 0.01:
+            p = self.Kp * self.error
+            i = self.Ki * self.error_sum
+            d = self.Kd * self.change_in_error
+            self.velocity.linear.x = p + i + d
+        else:
+            self.velocity.linear.x, p, i = 0.0, 0.0, 0.0
+            self.prev_error, self.error, d = 0.0, 0.0, 0.0
+            self.error_sum, self.change_in_error = 0.0, 0.0
 
-        # print(self.velocity.linear.x)
+        # Publishing Velocity
         self.publisher.publish(self.velocity)
 
         # Position of the Bot
-        print("----------------------------")
-        print("----------Position----------")
+        print("-----------------------------------------")
+        print("----------Position (in radians)----------")
         print("x = %.5f, y = %.5f, z = %.5f" % \
             (msg.pose.pose.position.x, msg.pose.pose.position.y,
              msg.pose.pose.position.z))
         # Quaternion Position
-        print("---------Quaternion---------")
+        print("---------Quaternion (in radians)---------")
         print("x = %.5f, y = %.5f, z = %.5f, w = %.5f" % \
             (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
              msg.pose.pose.orientation.z, msg.pose.pose.orientation.w))
         # Euler Angles
-        print("-------roll pitch yaw-------")
-        print(math.degrees(self.curr_pos), math.degrees(p), math.degrees(y))
+        print("-------roll pitch yaw (in radians)-------")
+        print("r = %.5f, p = %.5f, y = %.5f" % (self.curr_pos, p, y))
+        print("-------roll pitch yaw (in degrees)-------")
+        print("r = %.5f, p = %.5f, y = %.5f" % (math.degrees(self.curr_pos),
+                                                math.degrees(p), math.degrees(y)))
         # PID Values
-        print("---------PID Values---------")
+        print("-----------------P I D-------------------")
+        print("Pitch Error = %f" % self.error)
         print("Previous Error = %f" % self.prev_error)
         print("Error Sum = %f" % self.error_sum)
-        print("PID = %f" % self.velocity.linear.x)
         print("Change in Error = %f" % self.change_in_error)
-        print("Pitch Error = %f" % self.pitch_error)
+        print("PID = %f" % self.velocity.linear.x)
+        print("p = %.5f, i = %.5f, d = %.5f" % (p, i, d))
+        print("----------------------------------------\n")
+
+        # Updating pervious errors
+        self.prev_error = self.error
 
 # Main Function
 def main():
