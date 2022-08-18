9d9981cf37046285e4a0ef8fbb0a78f34c4bbd4a
hari-vickey <harivikinesh@gmail.com>
2022-08-18 11:31:21 +0530

    Updated Friction

--- bd1_self_balancer/bd1_self_balancer/self_balancer.py
+++ bd1_self_balancer/bd1_self_balancer/self_balancer.py
@@ -25,8 +25,7 @@ class SelfBalancingBot(Node):
         # Initializing Node
         super().__init__('self_balancer')
         # PID Variables
-        self.set_pos, self.limit = 0.0, 100
-        self.Kp, self.Ki, self.Kd = 33, 1e-5, 50
+        self.set_pos, self.Kp, self.Ki, self.Kd = 0.0, 2, 0, 300
         self.curr_pos, self.prev_error, self.error_sum = 1e-4, 0, 0
 
         self.publisher = self.create_publisher(Twist, '/cmd_vel', 1)
@@ -57,30 +56,24 @@ class SelfBalancingBot(Node):
         # Computing Sum of errors (For Integral Term)
         self.error_sum += self.error
 
-        # Limiting Error Sum
-        if int(self.error_sum) not in range(-self.limit, self.limit):
-            if self.error_sum <= 0:
-                self.error_sum = self.error_sum + self.limit
-            else:
-                self.error_sum = self.error_sum - self.limit
-
         # Computing the output of controller
         if abs(self.error) > 0.01:
             p = self.Kp * self.error
             i = self.Ki * self.error_sum
             d = self.Kd * self.change_in_error
-            self.velocity.linear.x = p + i + d
+            pid = p + i + d
+            self.velocity.linear.x = pid*math.exp(4)
         else:
-            self.velocity.linear.x, p, i = 0.0, 0.0, 0.0
-            self.prev_error, self.error, d = 0.0, 0.0, 0.0
             self.error_sum, self.change_in_error = 0.0, 0.0
+            self.prev_error, self.error, pid = 0.0, 0.0, 0.0
+            self.velocity.linear.x, p, i, d = 0.0, 0.0, 0.0, 0.0
 
         # Publishing Velocity
         self.publisher.publish(self.velocity)
 
         # Position of the Bot
         print("-----------------------------------------")
-        print("----------Position (in radians)----------")
+        print("-----------Position (in meter)-----------")
         print("x = %.5f, y = %.5f, z = %.5f" % \
             (msg.pose.pose.position.x, msg.pose.pose.position.y,
              msg.pose.pose.position.z))
